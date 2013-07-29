407aa10f726f6462dc50d21cbe3b9741067fceab
JoshIzaac <JoshIzaac@8e979c15-8541-47ad-9494-35a50ab3f3bd>
2013-07-29 18:25:02 +0000

    * added 2P PETSc chebyshev implementation

--- src/slepc/slepc.F90
+++ src/slepc/slepc.F90
@@ -1,9 +1,12 @@
 module eigs
+
 #include <finclude/petscsys.h>
 #include <finclude/petscvec.h>
 #include <finclude/petscmat.h>
 #include <finclude/petscpc.h>
 #include <finclude/petscksp.h>
+#include <finclude/petscvec.h90>
+
 #include <finclude/slepcsys.h>
 #include <finclude/slepceps.h>
 #include <finclude/slepcmfn.h>
@@ -13,9 +16,8 @@ module eigs
     ! create a sparse hamiltonian matrix of size n
     ! using PETSc's sparse matrix routines
     subroutine hamiltonian_1p(A,d,amp,nd,n)
-        PetscInt, intent(in)    :: nd, n
-        real(8), intent(in)     :: amp(nd)
-        integer, intent(in)     :: d(nd)
+        PetscInt, intent(in)    :: nd, n, d(nd)
+        PetscScalar, intent(in) :: amp(nd)
         
         Mat, intent(out)        :: A
         
@@ -34,6 +36,14 @@ module eigs
         if (Istart == 0) then
             col(1:2) = [0,1]
             value(1:2) = [2.0,-1.0]
+            
+            do j = 1, nd
+                if (int(d(j)+n/2.0) == 1) then
+                    value(1) = 2.0+amp(j)
+                    exit
+                endif
+            end do
+            
             call MatSetValues(A,1,0,2,col,value,INSERT_VALUES,ierr)
             Istart = Istart + 1
         end if
@@ -41,6 +51,14 @@ module eigs
         if (Iend == n) then
             col(1:2) = [n-2,n-1]
             value(1:2) = [-1.0,2.0]
+            
+            do j = 1, nd
+                if (int(d(j)+n/2.0) == n) then
+                    value(2) = 2.0+amp(j)
+                    exit
+                endif
+            end do
+            
             call MatSetValues(A,1,n-1,2,col,value,INSERT_VALUES,ierr)
             Iend = Iend - 1
         endif
@@ -48,6 +66,15 @@ module eigs
         value(1:3) = [-1.0,2.0,-1.0]
         do i=Istart, Iend-1
             col = [i-1,i,i+1]
+            
+            value = [-1.0,2.0,-1.0]
+            do j = 1, nd
+                if (int(d(j)+n/2.0) == i+1) then
+                    value(2) = 2.0+amp(j)
+                    exit
+                endif
+            end do
+            
             call MatSetValues(A,1,i,3,col,value,INSERT_VALUES,ierr)
         enddo
         
@@ -55,6 +82,128 @@ module eigs
         call MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY,ierr)
     
     end subroutine hamiltonian_1p
+    
+    
+    ! create a sparse 2P hamiltonian matrix of size n^2 x n^2
+    ! using PETSc's sparse matrix routines
+    subroutine hamiltonian_2p(H1,H2,n)
+        PetscInt, intent(in)    :: n
+        Mat, intent(in)         :: H1
+        
+        Mat, intent(out)        :: H2
+        
+        ! local variables
+        VecScatter     :: ctx
+        PetscErrorCode :: ierr
+        PetscBool      :: flag
+        PetscInt       :: NN, i, j, its, Istart, Iend, col(3)
+        PetscScalar    :: value(3)
+        Mat            :: temp
+        Vec            :: diag, diagAll
+        PetscScalar, pointer :: diagArray(:)
+        
+        NN = n**2
+        
+        ! create H2 matrix
+        call MatSetSizes(H2,PETSC_DECIDE,PETSC_DECIDE,NN,NN,ierr)
+        call MatSetFromOptions(H2,ierr)
+        call MatSetUp(H2,ierr)
+        
+        ! create temp matrix
+        !call MatDuplicate(H2,MAT_DO_NOT_COPY_VALUES,temp,ierr)
+        call MatCreate(PETSC_COMM_WORLD,temp,ierr)
+        call MatSetSizes(temp,PETSC_DECIDE,PETSC_DECIDE,NN,NN,ierr)
+        call MatSetFromOptions(temp,ierr)
+        call MatSetUp(temp,ierr)
+        
+        ! create vector of diagonal entries
+        call VecCreateMPI(PETSC_COMM_WORLD,PETSC_DECIDE,n,diag,ierr)
+        call VecSetFromOptions(diag,ierr)
+        call MatGetDiagonal(H1,diag,ierr)
+        
+        ! scatter the vector to an array on *all* nodes
+        call VecScatterCreateToAll(diag,ctx,diagAll,ierr)
+        call VecScatterBegin(ctx,diag,diagAll,INSERT_VALUES,SCATTER_FORWARD,ierr)
+        call VecScatterEnd(ctx,diag,diagAll,INSERT_VALUES,SCATTER_FORWARD,ierr)
+        call VecScatterDestroy(ctx)
+
+        call VecGetArrayF90(diagAll,diagArray,ierr)
+        
+        ! create KronProd(H1,I) matrix and store in temp
+        call MatGetOwnershipRange(temp,Istart,Iend,ierr)
+        
+        if (Istart == 0) then
+            col(1:2) = [0,1]
+            value(1:2) = [diagArray(1),-1.d0+0*PETSC_i]            
+            call MatSetValues(temp,1,0,2,col,value,INSERT_VALUES,ierr)
+            Istart = Istart + 1
+        end if
+        
+        if (Iend == NN) then
+            col(1:2) = [NN-2,NN-1]
+            value(1:2) = [-1.d0+0*PETSC_i,diagArray(n)]            
+            call MatSetValues(temp,1,NN-1,2,col,value,INSERT_VALUES,ierr)
+            Iend = Iend - 1
+        endif
+        
+        do i=Istart, Iend-1
+            if (mod(1,n)+1==1) then
+            
+                col(1:2) = [i,i+1]
+                value(1:2) = [diagArray(mod(i,n)+1),-1.d0+0*PETSC_i]            
+                call MatSetValues(temp,1,i,2,col,value,INSERT_VALUES,ierr)
+            
+            elseif (mod(i,n)+1==n) then
+            
+                col(1:2) = [i-1,i]
+                value(1:2) = [-1.d0+0*PETSC_i,diagArray(mod(i,n)+1)]            
+                call MatSetValues(temp,1,i,2,col,value,INSERT_VALUES,ierr)
+            
+            else
+                col = [i-1,i,i+1]
+                value = [-1.d0+0*PETSC_i,diagArray(mod(i,n)+1),-1.d0+0*PETSC_i]            
+                call MatSetValues(temp,1,i,3,col,value,INSERT_VALUES,ierr)
+            endif
+        enddo
+        
+        call MatAssemblyBegin(temp,MAT_FINAL_ASSEMBLY,ierr)
+        call MatAssemblyEnd(temp,MAT_FINAL_ASSEMBLY,ierr)
+        
+        ! create KronProd(I,H1) matrix and store in H2
+        call MatGetOwnershipRange(H2,Istart,Iend,ierr)
+        
+        do while (Istart<n)
+            col(1:2) = [Istart,Istart+n]
+            value(1:2) = [diagArray(1),-1.d0+0*PETSC_i]            
+            call MatSetValues(H2,1,Istart,2,col,value,INSERT_VALUES,ierr)
+            Istart = Istart + 1
+        end do
+        
+        do while (Iend>NN-n)
+            col(1:2) = [Iend-2-n,Iend-1]
+            value(1:2) = [-1.d0+0*PETSC_i,diagArray(n)]            
+            call MatSetValues(H2,1,Iend-1,2,col,value,INSERT_VALUES,ierr)
+            Iend = Iend - 1
+        end do
+        
+        do i=Istart, Iend-1
+            col = [i-n,i,i+n]
+            value = [-1.d0+0*PETSC_i,diagArray(int(real(i)/6.0)+1),-1.d0+0*PETSC_i]
+            call MatSetValues(H2,1,i,3,col,value,INSERT_VALUES,ierr)
+        enddo
+        
+        call MatAssemblyBegin(H2,MAT_FINAL_ASSEMBLY,ierr)
+        call MatAssemblyEnd(H2,MAT_FINAL_ASSEMBLY,ierr)
+        
+        call VecRestoreArrayF90(diag,diagArray,ierr)
+        call VecDestroy(diag,ierr)
+        
+        ! create 2P hamltonian H2=KronProd(I,H1)+KronProd(H1,I)
+        call MatAXPY(H2,1.0+0*PETSC_i,temp,DIFFERENT_NONZERO_PATTERN,ierr)
+        
+        call MatDestroy(temp,ierr)
+    
+    end subroutine hamiltonian_2p
     
     ! calculate y=e^(-iHt).v using SLEPc    
     subroutine expm(A,t,v,y)
@@ -77,6 +226,7 @@ module eigs
         call MatScale(A,alpha,ierr)
         !call MFNSetScaleFactor(mfn, alpha,ierr)
         call MFNSolve(mfn,v,y,ierr)
+        call MatScale(A,1.0/alpha,ierr)
         
         call MFNDestroy(mfn,ierr)
     
@@ -118,16 +268,16 @@ module eigs
             if (max_it .ne. 0) then
                 call EPSSetTolerances(eps,tolIn,max_it,ierr)
             else
-                call EPSSetTolerances(eps,tolIn,PETSC_NULL_OBJECT,ierr)
+                call EPSSetTolerances(eps,tolIn,PETSC_DECIDE,ierr)
             endif
         else
-            if (max_it .ne. 0) call EPSSetTolerances(eps,PETSC_NULL_OBJECT,max_it,ierr)
+            if (max_it .ne. 0) call EPSSetTolerances(eps,PETSC_DECIDE,max_it,ierr)
         endif
         
         ! set the workspace options
         select case(worktype)
-            case('mpd');   call EPSSetDimensions(eps,1,PETSC_NULL_OBJECT,worktypeInt,ierr)
-            case('ncv');   call EPSSetDimensions(eps,1,worktypeInt,PETSC_NULL_OBJECT,ierr)
+            case('mpd');   call EPSSetDimensions(eps,1,PETSC_DECIDE,worktypeInt,ierr)
+            case('ncv');   call EPSSetDimensions(eps,1,worktypeInt,PETSC_DECIDE,ierr)
             case default;  continue ! SLEPC default workspace
         end select
         
@@ -144,7 +294,7 @@ module eigs
             case default;        continue ! SLEPC default is Krylov-Schur
         end select        
 
-	! determine whether min or max Re(lambda) is determined
+    ! determine whether min or max Re(lambda) is determined
         select case(which)
             case('max');
                 if (rank==0) write(*,*)'Calculating Emax...'
@@ -188,7 +338,7 @@ module eigs
         if (nconv==0) then
             error = 1
             if (rank==0) write(*,*)'No converged eigenpairs. Reduce tolerance, increase &
-        				& number of interations, or increase work vector size'
+                        & number of interations, or increase work vector size'
         else
             call EPSGetEigenpair(eps,0,kr,ki,PETSC_NULL_OBJECT,PETSC_NULL_OBJECT,ierr)
             call EPSComputeRelativeError(eps,0,Eval_error,ierr)
@@ -199,20 +349,80 @@ module eigs
         call EPSDestroy(eps,ierr)
         
     end subroutine min_max_eigs
+    
+    subroutine qw_cheby(psi0,psi,dt,H,Emin,Emax,rank,N)
+        PetscMPIInt, intent(in) :: rank
+        PetscInt, intent(in)    :: N
+        PetscScalar, intent(in) :: dt, Emin, Emax
+        Vec, intent(in)         :: psi0
+        Mat, intent(in)         :: H
+        
+        Vec, intent(out)        :: psi
+        
+        ! local variables
+        PetscInt      :: m, terms, i, j
+        PetscReal     :: alpha
+        PetscScalar   :: bessj0, bessj1, bessjn
+        Vec, pointer  :: work(:)
+        
+        alpha = PetscRealPart((Emax-Emin)*dt/2.0)
+        
+        call VecDuplicateVecsF90(psi0,4,work,ierr)
+        
+        call VecCopy(psi0,work(1),ierr)
+        call MatMult(H,work(1),work(2),ierr)
+        call VecAXPBY(work(2), (Emax+Emin)/(Emax-Emin),-2.0/(Emax-Emin), work(1),ierr)
+        
+        bessj0 = dbesjn(0,alpha)
+        bessj1 = dbesjn(1,alpha)
+        call VecCopy(psi0,work(4),ierr)
+        call VecAXPBY(work(4), 2.0*PETSC_i*bessj1,bessj0, work(2),ierr)
+        
+        terms = 0
+        do while (abs(2.d0*dbesjn(terms,alpha)) > 1.d-18)
+            terms = terms + 1
+        end do
+        
+        do m = 2, terms
+            call MatMult(H,work(2),work(3),ierr)
+            call VecAXPBY(work(3), 2.0*(Emax+Emin)/(Emax-Emin),-4.0/(Emax-Emin), work(2),ierr)
+            call VecAXPY(work(3),-1.0+0.*PETSC_i,work(1),ierr)
+            
+            bessjn = dbesjn(m,alpha)
+            call VecAXPY(work(4),2.0*(PETSC_i**m)*bessjn,work(3),ierr)
+
+            call VecCopy(work(2),work(1),ierr)
+            call VecCopy(work(3),work(2),ierr)
+        end do
+        
+        call VecScale(work(4),exp(-PETSC_i*(Emax+Emin)*dt/2.0),ierr)
+        call VecCopy(work(4),psi,ierr)
+        
+        call VecDestroyVecsF90(4,work,ierr)
+    
+    end subroutine qw_cheby
 
 end module eigs
+
+!~~~~~~~~~~~~~~~~~~ Todo ~~~~~~~~~~~~~~~~~~~~~~
+!  1) Marginal prob subroutine
+!  2) Create intial statespace subroutine
+!       -this would require a coord function
+!  3) Test with f2py
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 program slepc
 
     use eigs
     
+    PetscViewer    :: matlabV
     PetscMPIInt    :: rank
     PetscErrorCode :: ierr
     PetscBool      :: flag
-    PetscInt       :: i, j, its, n
-    PetscScalar    :: Emin, Emax, t, value(3)
+    PetscInt       :: i, j, its, n, d(4)
+    PetscScalar    :: Emin, Emax, t, value(3), amp(4)
     PetscReal      :: Emin_error, Emax_error
-    Mat            :: A
+    Mat            :: H1, H2
     Vec            :: psi0, psi
     
     ! initialize SLEPc and PETSc
@@ -224,22 +434,30 @@ program slepc
     CHKERRQ(ierr)
     if (flag .eqv. .false.) n = 10
     
-    ! matrix creater
-    call MatCreate(PETSC_COMM_WORLD,A,ierr)
-    call hamiltonian_1p(A,[0],[0.d0],1,n)
-    !call MatView(A,PETSC_VIEWER_STDOUT_SELF)
+    ! 1p matrix creater
+    d = [-2,3,0,2]
+    amp = [-0.50,5.0,1.0,0.50]
+    call MatCreate(PETSC_COMM_WORLD,H1,ierr)
+    call hamiltonian_1p(H1,d,amp,size(d),n)
+    !call MatView(H1,PETSC_VIEWER_STDOUT_WORLD,ierr)
+    
+    ! 2p matrix creater
+    call MatCreate(PETSC_COMM_WORLD,H2,ierr)
+    call hamiltonian_2p(H1,H2,n)
+    !call MatView(H2,PETSC_VIEWER_STDOUT_WORLD,ierr)
     
     ! Eigenvalue solver
-    call min_max_eigs(A,rank,Emin,Emin_error,'min','krylovschur','mpd',50,0.d0,0,.true.,ierr)
+    call min_max_eigs(H2,rank,Emin,Emin_error,'min','krylovschur','mpd',50,0.d0,0,.false.,ierr)
     if (rank==0 .and. ierr==0) write(*,*)'    ',PetscRealPart(Emin),'+-', Emin_error
-    call min_max_eigs(A,rank,Emax,Emax_error,'max','krylovschur','mpd',50,0.d0,0,.true.,ierr)
+    call min_max_eigs(H2,rank,Emax,Emax_error,'max','krylovschur','mpd',50,0.d0,0,.false.,ierr)
     if (rank==0 .and. ierr==0) write(*,*)'    ',PetscRealPart(Emax),'+-',Emax_error
     
     ! create vectors
-    call VecCreateMPI(PETSC_COMM_WORLD,PETSC_DECIDE,n,psi0,ierr)
+    call VecCreateMPI(PETSC_COMM_WORLD,PETSC_DECIDE,n**2,psi0,ierr)
     call VecSetFromOptions(psi0,ierr)
     call VecDuplicate(psi0,psi,ierr)
     
+    ! create initial state
     value = [1.0,2.0,-0.5]
     call VecSetValues(psi0,3,[0,1,2],value,INSERT_VALUES,ierr)
     call VecAssemblyBegin(psi0,ierr)
@@ -247,11 +465,19 @@ program slepc
     
     ! matrix exponential
     t = 5.0
-    call expm(A,t,psi0,psi)
+    call expm(H2,t,psi0,psi)
     !call VecView(psi,PETSC_VIEWER_STDOUT_WORLD,ierr)
+    
+    ! QW chebyshev
+    call qw_cheby(psi0,psi,t,H2,Emin,Emax,rank,n**2)
+    call VecView(psi,PETSC_VIEWER_STDOUT_WORLD,ierr)
 
     ! destroy matrix/SLEPc
-    call MatDestroy(A,ierr)
+    call MatDestroy(H1,ierr)
+    call MatDestroy(H2,ierr)
+    call VecDestroy(psi,ierr)
+    call VecDestroy(psi0,ierr)
+    
     call SlepcFinalize(ierr)
 
 end program slepc
