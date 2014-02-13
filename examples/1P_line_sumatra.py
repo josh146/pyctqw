2a27a0ad6c439ffc9f1d0a3a513cd6f160f2d97b
josh146 <josh146@gmail.com>
2014-02-13 07:33:42 -0800

    fixed relative filename bug in sumatra

--- examples/1P_line_sumatra.py
+++ examples/1P_line_sumatra.py
@@ -11,6 +11,8 @@ from sumatra.decorators import capture
 # import pyCTQW as qw
 import pyCTQW.MPI as qw
 
+__file__ = "trunk/examples/1P_line_sumatra.py"
+
 @capture
 def main(parameters):
 	# get the MPI rank
